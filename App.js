cbe5831a3c14ef2a34e94bb8d50851addf8706f5
Abrahamcepe <abrahamgolf@gmail.com>
2020-12-24 11:18:43 -0600

    Initial setup of screens

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-native/native'
+import { createStackNavigation } from '@react-navigation/stack'
+
+const Stack = createStackNavigation()
+
+function myStack(){
+  return (
+    <Stack.Navigator>
+      <Stack.screen/>
+      <Stack.screen/>
+      <Stack.screen/>
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+    </NavigationContainer>
   );
 }
 
---
+++ screens/CreateUser.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function CreateUser() {
+    return (
+        <View>
+            <Text>Create User</Text>
+        </View>
+    )
+}
